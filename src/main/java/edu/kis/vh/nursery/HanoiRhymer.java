466e4d9247787839a2ed26b516700328b4e4b2ca
Mvteusz01 <251194@edu.p.lodz.pl>
2025-04-08 21:33:26 +0200

    17. Wygenerowano szkielet dokumentacji dla klasy HanoiRhymer oraz jej metetod

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,12 +1,31 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Rhymer typu Hanoi, który odrzuca wartości większe od aktualnie znajdującej się na szczycie stosu.
+ * Zlicza wszystkie takie przypadki jako odrzucone.
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
+    /**
+     * Liczba wartości odrzuconych przy próbie dodania do stosu.
+     */
     int totalRejected = 0;
 
+    /**
+     * Zwraca łączną liczbę odrzuconych wartości.
+     *
+     * @return liczba odrzuceń
+     */
     public int reportRejected() {
         return totalRejected;
     }
+
+    /**
+     * Dodaje wartość do stosu, jeśli jest mniejsza lub równa aktualnej wartości na szczycie.
+     * W przeciwnym razie zwiększa licznik odrzuceń.
+     *
+     * @param in wartość do dodania
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaBoo())
